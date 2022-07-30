de00eb3bd873a06767fb58c6409dd1eb06941524
Leo Lungu <leo.lungu13@yahoo.com>
2022-07-30 23:47:37 +0300

    Added GUI
    Added working GUI
    Need to make it more user friendly and more appealing, right now the UX isn't that aesthetically pleasing

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const container = document.querySelector('.results');
+const round = document.getElementById("round");
+const scorePlayer = document.getElementById('scorePlayer');
+const scoreComputer = document.getElementById('scoreComputer');
+const winner = document.getElementById('winner');
+
 let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3));
@@ -10,6 +17,7 @@ function getComputerChoice() {
     } else if (choice == 2) {
         choiceString = "scissors";
     }
+    document.getElementById("computerImg").src="images/" + choiceString + ".png";
     return choiceString;
 }
 
@@ -17,16 +25,19 @@ function playRound(playerSelection, computerSelection) {
     let winner = "default";
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
         winner = "You lose! Paper beats rock!";
+        computerScore++;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
         winner = "You win! Paper beats rock!";
         playerScore++;
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
         winner = "You lose! Scissors beats paper!";
+        computerScore++;
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
         winner = "You win! Rock beats scissors!";
         playerScore++;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
         winner = "You lose! Rock beats scissors!";
+        computerScore++;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
         winner = "You win! Scissors beats paper!";
         playerScore++;
@@ -39,15 +50,51 @@ function playRound(playerSelection, computerSelection) {
     return winner;
 }
 
-function game() {
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = "";
-        playerSelection = prompt("Rock, Paper or Scissors?");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    console.log(playerScore);
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    round.classList.add('round');
+    playerSelection = "rock";
+    winnerRound(round);
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    round.classList.add('round');
+  playerSelection = "paper";
+  winnerRound(round);
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    round.classList.add('round');
+  playerSelection = "scissors";
+  winnerRound(round);
+});
+
+function winnerRound(round) {
+    winner.textContent = "Winner: ?";
+    document.getElementById("playerImg").src="images/" + playerSelection + ".png";
+    round.textContent = playRound(playerSelection, getComputerChoice());
+        scorePlayer.textContent = ("Player: " + playerScore);
+        scoreComputer.textContent = ("Computer: " + computerScore);
+        console.log(checkEnd());
 }
 
-game();
+function checkEnd() {
+    if (playerScore == 5 || computerScore == 5) {
+        winner.textContent = ("Winner: " + checkWinner());
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
+
+function checkWinner() {
+    let winner = "Winner: ?";
+
+    if (playerScore > computerScore) {
+        winner = "Player";
+    } else {
+        winner = "Computer";
+    }
+    return winner;
+}
